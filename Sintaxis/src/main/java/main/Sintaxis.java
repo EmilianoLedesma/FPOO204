a0f3134c298cf7263d87f18e4b73932a780bd379
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2025-01-21 17:14:12 -0600

    Se cambio el texto a imprimir

--- Sintaxis/src/main/java/main/Sintaxis.java
+++ Sintaxis/src/main/java/main/Sintaxis.java
@@ -7,6 +7,6 @@ package main;
 public class Sintaxis {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Emiliano Ledesma");
     }
 }
