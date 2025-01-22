feebb7a36439ec0c346c46d179d909db87f81025
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2025-01-22 14:40:06 -0600

    Se extrajó información de las cadenas

--- Sintaxis/src/App.java
+++ Sintaxis/src/App.java
@@ -1,7 +1,24 @@
+// IMPORTACIONES
 
+//Clase Principal
 public class App {
 
+    //Método Main
+    /*Soy
+     * Un
+     * Chivista de corazon
+     * arriba las chivas
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Emiliano");
+        String cadenas = "Emiliano" + " Ledesma" + " Ledesma";
+
+        System.out.println(cadenas);
+        System.out.println(cadenas.length());
+        //Extraer información de cadenas
+        System.out.println(cadenas.substring(2, 5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0, 5));
+
     }
 }
