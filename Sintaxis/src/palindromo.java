6e37ec951f824ec4848b4939496fd7992cb51f81
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2025-01-28 16:24:07 -0600

    Palindromo
    Codifica un programa que solicite una cadena de caracteres e imprima como
    resultado si la cadena es palíndromo o no

---
+++ Sintaxis/src/palindromo.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class palindromo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una cadena de caracteres:");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (esPalindromo(input)) {
+            System.out.println("La cadena " + input + " es un palíndromo.");
+        } else {
+            System.out.println("La cadena no es un palíndromo.");
+        }
+    }
+
+    public static boolean esPalindromo(String cadena) {
+        cadena = cadena.toLowerCase();
+        String invertida = new StringBuilder(cadena).reverse().toString();
+        return invertida.equals(cadena);
+    }
+}
